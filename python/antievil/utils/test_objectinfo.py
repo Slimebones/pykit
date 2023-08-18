82e97a0865afd577c5eb60d3c2389937125cb0a3
ryzhovalex <thed4rkof@gmail.com>
2023-08-18 12:43:43 +0300

    feat(utils): one var initialization for object info

--- python/antievil/utils/test_objectinfo.py
+++ python/antievil/utils/test_objectinfo.py
@@ -5,6 +5,12 @@ def test_main():
     info: ObjectInfo = ObjectInfo[int](("hello", 2))
     assert str(info) == "hello=<2>"
 
+
+def test_only_title():
+    info: ObjectInfo = ObjectInfo[int]("hello")
+    assert str(info) == "hello"
+
+
 def test_overflow():
     try:
         ObjectInfo[int](("hello", 1, "whocares"))  # type: ignore
