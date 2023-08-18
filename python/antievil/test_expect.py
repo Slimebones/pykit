2a7964bffa07179526b5a09a5669942e4e2e287e
ryzhovalex <thed4rkof@gmail.com>
2023-08-18 13:08:54 +0300

    feat(expect): length expect

--- python/antievil/test_expect.py
+++ python/antievil/test_expect.py
@@ -1,4 +1,4 @@
-from antievil._expect import NameExpectError
+from antievil._expect import LengthExpectError, NameExpectError
 
 
 def test_name():
@@ -8,3 +8,17 @@ def test_name():
     )
 
     assert error.args[0] == "goodbye=<1> expected to have name <hello>"
+
+
+def test_length():
+    input_iterable: list[int] = [1, 2, 3]
+    error: LengthExpectError = LengthExpectError(
+        input_iterable,
+        5,
+    )
+
+    assertion_message: str = \
+        f"iterable <{input_iterable}> expected to be of" \
+        f" length <5>, got length <3>"
+
+    assert error.args[0] == assertion_message
