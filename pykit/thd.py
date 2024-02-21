e11005240d34215ae8f0ee49b44e021a9dad882e
ryzhovalex <thed4rkof@gmail.com>
2024-02-21 13:10:39 +0300

    finish thd

--- pykit/thd.py
+++ pykit/thd.py
@@ -4,11 +4,17 @@ Transaction handling aka THD.
 
 from asyncio import Queue
 import inspect
-from typing import Any, Callable, Coroutine, Self
+from typing import Any, Awaitable, Callable, Coroutine, Self
 
 from pydantic import BaseModel
 from pykit.log import log
-from pykit.err import LockErr
+from pykit.types import T
+from pykit.err import InpErr, LockErr
+
+_RollbackFnAndPreResult = tuple[
+    Callable[[Any], Awaitable[None] | None],
+    Any
+]
 
 class Thd:
     # todo:
@@ -17,7 +23,7 @@ class Thd:
     #           "defer" => execute only on commit
     def __init__(self):
         self._is_queue_locked = False
-        self._rollback_queue = Queue()
+        self._rollback_queue = Queue[_RollbackFnAndPreResult]()
 
     async def __aenter__(self) -> Self:
         return self
@@ -31,45 +37,40 @@ class Thd:
         self._is_queue_locked = True
         if err_val:
             while not self._rollback_queue.empty():
-                fn = await self._rollback_queue.get()
+                fn, preresult = await self._rollback_queue.get()
                 try:
                     if inspect.iscoroutine(fn):
-                        await fn
-                        return
+                        raise InpErr(f"expected corofn, but coroutine {fn}")
                     if inspect.iscoroutinefunction(fn):
-                        log.warn(
-                            "you should pass either callable or coro,"
-                            f" got coroutine function {fn} => execute anyway"
-                        )
-                        await fn()
+                        await fn(preresult)
                         return
-                    fn()
+                    fn(preresult)
                 except Exception as err:
                     log.warn(
-                        "catch err during rollback, during execution of"
+                        "catch err (below) during rollback, during execution of"
                         f" fn {fn} => continue"
                     )
                     log.catch(err)
 
     def a(
         self,
-        fn: Callable,
-        rollback_fn: Callable
+        fn: Callable[[], T],
+        rollback_fn: Callable[[T], None]
     ) -> Any:
         if self._is_queue_locked:
             raise LockErr("thd queue")
         f = fn()
-        self._rollback_queue.put_nowait(rollback_fn)
+        self._rollback_queue.put_nowait((rollback_fn, f))
         return f
 
     async def aa(
         self,
-        fn: Coroutine,
-        rollback_fn: Coroutine
+        fn: Coroutine[Any, Any, T],
+        rollback_corofn: Callable[[T], Awaitable[None]]
     ) -> Any:
         if self._is_queue_locked:
             raise LockErr("thd queue")
         f = await fn
-        await self._rollback_queue.put(rollback_fn)
+        await self._rollback_queue.put((rollback_corofn, f))
         return f
 
