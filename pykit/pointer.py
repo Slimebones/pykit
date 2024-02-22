60c78d51f36daeed840bf12dfcecf6c7b1c2934e
ryzhovalex <thed4rkof@gmail.com>
2024-02-22 13:47:04 +0300

    add pointer

---
+++ pykit/pointer.py
@@ -0,0 +1,16 @@
+from typing import Any, Generic
+
+from pydantic.generics import GenericModel
+
+from pykit.types import T
+
+
+class Pointer(GenericModel, Generic[T]):
+    """
+    Points to some target.
+    """
+    target: T
+
+    class Config:
+        arbitrary_types_allowed = True
+
