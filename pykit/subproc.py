d2767554f8affec6c181119d9687546dff64d940
ryzhovalex <thed4rkof@gmail.com>
2024-02-23 13:37:19 +0300

    add subproc

---
+++ pykit/subproc.py
@@ -0,0 +1,41 @@
+import subprocess
+from typing import Callable
+
+from pykit.log import log
+
+
+class SubprocUtils:
+    @classmethod
+    def popen(
+        cls,
+        cmd: str,
+        fn: Callable[[subprocess.Popen], None],
+        *,
+        must_raise_retcode_err: bool = True
+    ):
+        """
+        Creates popen and calls given fn with it.
+
+        Fn to read stdout example:
+        ```python
+        def myfn(proc):
+            if proc.stdout:
+                for line in proc.stdout:
+                    print(line, end="")  # do whatever you want
+        ```
+        """
+        with subprocess.Popen(
+            cmd,
+            stdout=subprocess.PIPE,
+            text=True,
+            shell=True
+        ) as process:
+            try:
+                fn(process)
+            except KeyboardInterrupt:
+                log.info("receive keyboard interrupt => kill subproc")
+                process.kill()
+                raise KeyboardInterrupt()
+        if must_raise_retcode_err and process.returncode != 0:
+            raise subprocess.CalledProcessError(process.returncode, process.args)
+
