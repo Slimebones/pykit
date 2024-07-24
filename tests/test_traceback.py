41b4740a659572d3f682d8a589eb8e75877a0b51
ryzhovalex <thed4rkof@gmail.com>
2024-07-24 13:54:13 +0300

    update test_traceback.py

--- tests/test_traceback.py
+++ tests/test_traceback.py
@@ -2,9 +2,9 @@ from pykit.err import ValErr
 from pykit.traceback import create_traceback, get_traceback_str
 
 
-def test_create_traceback_depth_1():
+def test_create_traceback_depth_0():
     def inner(err: Exception) -> Exception:
-        return create_traceback(err,1)
+        return create_traceback(err, 0)
 
     err = ValErr("hello")
     # use extra function for more informative stack
@@ -12,8 +12,20 @@ def test_create_traceback_depth_1():
     assert err is not new_err, "created side effects"
     tb_str = get_traceback_str(new_err)
     assert tb_str
-    # [-1] is "^^^^^^^^^^^^^^^", so we get [-2] for proper assertion
-    assert tb_str.split()[-2] == "create_traceback(err,1)"
+    assert tb_str.split()[-2] == "tb_lasti=next_frame.f_lasti,"
+
+def test_create_traceback_depth_1():
+    def inner(err: Exception) -> Exception:
+        return create_traceback(err, 1)
+
+    err = ValErr("hello")
+    # use extra function for more informative stack
+    new_err = inner(err)
+    assert err is not new_err, "created side effects"
+    tb_str = get_traceback_str(new_err)
+    assert tb_str
+    assert tb_str.split()[-3] == "create_traceback(err,"
+    assert tb_str.split()[-2] == "1)"
 
 def test_create_traceback_depth_2():
     def inner(err: Exception) -> Exception:
