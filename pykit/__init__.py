8d3b94725115e0fdbcf088d762b2e49c928ca18d
ryzhovalex <thed4rkof@gmail.com>
2024-07-29 15:54:58 +0300

    update __init__.py, pyproject.toml

--- pykit/__init__.py
+++ pykit/__init__.py
@@ -3,4 +3,4 @@ import importlib.metadata as _import_meta
 # We don't intend to export modules here since it would be cleaner due to
 # different modules to have them all expose themselves.
 
-__version__ = _import_meta.version("pykit")
+__version__ = _import_meta.version("sb-pykit")
